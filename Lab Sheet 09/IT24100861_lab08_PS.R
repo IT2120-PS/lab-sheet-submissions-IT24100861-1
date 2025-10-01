6fa805152837b99abe7f9d9eecbced6393a522e0
IT24100861 <IT24100861@my.sliit.lk>
2025-10-01 22:02:01 +0530

    Add files via upload

---
+++ Lab Sheet 09/IT24100861_lab08_PS.R
@@ -0,0 +1,10 @@
+setwd("C:\\Users\\aaa\\OneDrive\\Desktop\\IT----24100861")
+#Exercise
+#(01)
+       #i
+bake <- rnorm(25, mean = 45, sd = 2)
+bake
+
+      #ii
+t.test(bake, mu = 46, alternative = "less")
+
